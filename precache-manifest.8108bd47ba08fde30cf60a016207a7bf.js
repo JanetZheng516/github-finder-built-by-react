8ca034c0c5b7f0c300cbd3a16aa6cd4bf55ce567
JanetZheng516 <janet@pop-os.localdomain>
2020-09-20 22:05:56 +0200

    Updates

---
+++ precache-manifest.8108bd47ba08fde30cf60a016207a7bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9404d763a96ca79204948b15773185db",
+    "url": "/github-finder-built-by-react/index.html"
+  },
+  {
+    "revision": "40980c0fb0af10c5e5dc",
+    "url": "/github-finder-built-by-react/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "834e472c5b7ddd732a55",
+    "url": "/github-finder-built-by-react/static/js/2.75993eef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-finder-built-by-react/static/js/2.75993eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40980c0fb0af10c5e5dc",
+    "url": "/github-finder-built-by-react/static/js/main.3812669b.chunk.js"
+  },
+  {
+    "revision": "8009819a28779adb36c7",
+    "url": "/github-finder-built-by-react/static/js/runtime-main.abae809f.js"
+  }
+]);
